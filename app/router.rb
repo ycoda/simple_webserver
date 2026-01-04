5648439669408722e9801bba8ba5af4baeebeb95
ycoda <tankaku@gmail.com>
2026-01-04 14:38:09 +0900

    add: htmlファイルを表示できるようにした

--- app/router.rb
+++ app/router.rb
@@ -2,14 +2,24 @@ class Router
     def self.draw(server)
         @server = server
 
-        self.match('/', "Root page")
-        self.match('/hello', "Hello page")
-        self.match('/about', "About page")
+        self.match('/', "index.html")
+        self.match('/hello', "hello.html")
+        self.match('/about', "about.html")
     end
     
-    def self.match(path, message)
+    def self.match(path, filename)
         @server.mount_proc path do |req, res|
-            res.body = message
+            file_path = File.join('views', filename)
+            
+            
+            if File.exist?(file_path)
+                res.body = File.read(file_path)
+                res['Content-Type'] = 'text/html; charset=utf-8'
+            else
+                res.status = 404
+                res['Content-Type'] = 'text/html; charset=utf-8'
+                res.body = "404 not found #{filename}が見つかりません"
+            end
         end
     end
 end
