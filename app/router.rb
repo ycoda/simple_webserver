1ca2794e19762a94a990609762d8362404de10f0
ycoda <tankaku@gmail.com>
2026-01-04 14:54:05 +0900

    add:スタイルシートを適用できるようにした

--- app/router.rb
+++ app/router.rb
@@ -2,22 +2,25 @@ class Router
     def self.draw(server)
         @server = server
 
+        # HTMLの登録
         self.match('/', "index.html")
         self.match('/hello', "hello.html")
         self.match('/about', "about.html")
+        # CSSの登録
+        self.match('/style.css', 'style.css', 'text/css')
     end
     
-    def self.match(path, filename)
+    def self.match(path, filename, content_type = 'text/html')
         @server.mount_proc path do |req, res|
-            file_path = File.join('views', filename)
-            
+            folder = content_type == 'text/html' ? 'views' : 'public'
+            file_path = File.join(folder, filename)
             
             if File.exist?(file_path)
                 res.body = File.read(file_path)
-                res['Content-Type'] = 'text/html; charset=utf-8'
+                res['Content-Type'] = "#{content_type}; charset=utf-8"
             else
                 res.status = 404
-                res['Content-Type'] = 'text/html; charset=utf-8'
+                res['Content-Type'] = "#{content_type}; charset=utf-8"
                 res.body = "404 not found #{filename}が見つかりません"
             end
         end
